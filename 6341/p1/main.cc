364c0a6e17dbc1830a7a25707e4f8efa6c316e31
Freddy Gu <fgu@asymmetrik.com>
2017-09-01 15:30:44 -0400

    update

--- 6341/p1/main.cc
+++ 6341/p1/main.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "common.h"
-// #include "file1.h"
 
 using namespace std;
 
@@ -18,19 +18,15 @@ token_type getNextToken(string* input_string, int* index) {
 
   // Escape if the current position is at the end of an input line.
   if (*index >= (*input_string).length()) {
-    // cout << "in escape" << endl; // dev
     ret.type = EOF_ID;
     return ret;
   }
 
   char c = (*input_string)[*index];
-  // cout << "ASCII: " << int(c) << endl; // dev
 
   if (c == ' ') {
     (*index)++;
     while (*index < (*input_string).length() && (*input_string)[*index] == ' ') {
-      // c = (*input_string)[*index]; // dev
-      // cout << "ASCII: " << int(c) << endl; // dev
       (*index)++;
     }
     ret.type = *index == (*input_string).length() ? EOF_ID : 0;
@@ -44,15 +40,18 @@ token_type getNextToken(string* input_string, int* index) {
     ret.type = (c > 64 && c < 91) ? LITERAL_ATOM_ID : NUMERIC_ATOM_ID;
     int start_idx = *index;
     (*index)++;
-    while (*index < (*input_string).length() && (*input_string)[*index] != ' ') {
+    while (*index < (*input_string).length()) {
       c = (*input_string)[*index];
+      if (c < 48 || (c > 57 && c < 65) || c > 90) {
+        break;
+      }
       if (ret.type == NUMERIC_ATOM_ID && (c > 64 && c < 91)) {
         ret.type = ERROR_ID;
       }
       (*index)++;
     }
     int end_idx = *index;
-    string token = (*input_string).substr(start_idx, end_idx);
+    string token = (*input_string).substr(start_idx, end_idx - start_idx);
     if (ret.type == NUMERIC_ATOM_ID) {
       ret.numeral = stoi(token);
     } else {
@@ -68,24 +67,48 @@ token_type getNextToken(string* input_string, int* index) {
 
 int main() {
   string input_line;
+  int open_parenthesis_count = 0;
+  int close_parenthesis_count = 0;
+  int numeric_atom_count = 0;
+  int numeric_atom_sum = 0;
+  int literal_atom_count = 0;
+  vector<string> literal_atom_collection;
 
   while (getline(cin, input_line)) {
 
     int line_idx = 0;
-    int line_length = input_line.length();
-    cout << "INPUT: " << input_line << " LENGTH: " << line_length << endl;
 
     token_type tok = getNextToken(&input_line, &line_idx);
-    cout << "TOKEN TYPE: " << tok.type << endl;
     while (tok.type != ERROR_ID && tok.type != EOF_ID) {
-
+      if (tok.type == OPEN_PARENTHESIS_ID) {
+        open_parenthesis_count++;
+      } else if (tok.type == CLOSING_PARENTHESIS_ID) {
+        close_parenthesis_count++;
+      } else if (tok.type == NUMERIC_ATOM_ID) {
+        numeric_atom_count++;
+        numeric_atom_sum += tok.numeral;
+      } else if (tok.type == LITERAL_ATOM_ID) {
+        literal_atom_count++;
+        literal_atom_collection.push_back(tok.literal);
+      }
 
       tok = getNextToken(&input_line, &line_idx);
-      cout << "TOKEN TYPE: " << tok.type << endl;
     }
-    
-    cout << endl;
+
+    if (tok.type == ERROR_ID) {
+      cout << "ERROR: Invalid token " << tok.literal << endl;
+      return 0;
+    }
   }
+
+  cout << "LITERAL ATOMS: " << literal_atom_count;
+  for (vector<string>::iterator it = literal_atom_collection.begin(); it != literal_atom_collection.end(); ++it) {
+    cout << ", " << *it;
+  }
+  cout << endl;
+  cout << "NUMERIC ATOMS: " << numeric_atom_count << ", " << numeric_atom_sum << endl;
+  cout << "OPEN PARENTHESES: " << open_parenthesis_count << endl;
+  cout << "CLOSING PARENTHESES: " << close_parenthesis_count << endl;
 
   return 0;
 }
